a4574c75f2053a6814cad9451869ea9d414308b0
M. Lakshika <39967581+menulk@users.noreply.github.com>
2025-11-06 09:12:53 +0530

    Added menu and updated sale order view

--- __manifest__.py
+++ __manifest__.py
@@ -2,9 +2,14 @@
     'name': 'Odoo Reports',
     'version': '1.0',
     'category': 'Custom',
-    'summary': 'A simple custom module',
+    'summary': 'A custom module for odoo reports',
     'depends': ['base'],
     'data': [],
     'installable': True,
     'application': False,
+    'data': [
+    'views/menu.xml',
+    'views/sale_order_views.xml',
+    ],
 }
+
